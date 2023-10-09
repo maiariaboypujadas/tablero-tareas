87a2c8361f442553bc52d5f8592d62a10bccfc85
maiariaboypujadas <maiariaboy24@gmail.com>
2023-10-09 15:53:29 -0300

    cambios entre columnas

--- tablero-kanban/src/App.js
+++ tablero-kanban/src/App.js
@@ -9,7 +9,7 @@ function App() {
    
 
     <div className="App">
-      <h1>Hola</h1>
+      <h1>Proyectos</h1>
     <Home/>
     <Cards/>
     </div>
--- tablero-kanban/src/components/Cards.js
+++ tablero-kanban/src/components/Cards.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import Tarjeta from './Form';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
 
 const HomeContainer = styled.div`
   display: flex;
@@ -23,30 +24,166 @@ const TarjetaContainer = styled.div`
   padding: 10px;
   margin-bottom: 10px;
 `;
+const TareaForm = styled.form`
+  display: flex;
+  margin-top: 10px;
+`;
+
+const TareaInput = styled.input`
+  flex-grow: 1;
+  margin-right: 10px;
+`;
+
+const AgregarTareaButton = styled.button`
+  background-color: #333;
+  color: #fff;
+  border: none;
+  padding: 5px 10px;
+  cursor: pointer;
+`;
+
+const TareaItem = styled.li`
+  display: flex;
+  align-items: center;
+`;
+
+const TareaText = styled.span`
+  flex-grow: 1;
+`;
+
+const EliminarTareaButton = styled.button`
+  background-color: #ff0000;
+  color: #fff;
+  border: none;
+  padding: 5px 10px;
+  cursor: pointer;
+`;
 
 const Cards = () => {
-  const columnas = [
-    { titulo: 'Tareas', tarjetas: [] },
-    { titulo: 'En Progreso', tarjetas: [] },
-    { titulo: 'Completadas', tarjetas: [] },
-  ];
+  const [nuevaTarea, setNuevaTarea] = useState('');
+  const [columnas, setColumnas] = useState({
+    Tareas: [],
+    'En Progreso': [],
+    Completadas: []
+  });
+ 
+  const handleChange = (event) => {
+    setNuevaTarea(event.target.value);
+  };
 
+  const handleSubmit = (event, columna) => {
+    event.preventDefault();
+
+    if (nuevaTarea.trim() !== '') {
+      const nuevasTareas = [...columnas[columna], nuevaTarea];
+      const nuevasColumnas = {
+        ...columnas,
+        [columna]: nuevasTareas,
+      };
+      setColumnas(nuevasColumnas);
+      setNuevaTarea('');
+    }
+  };
+
+  const handleEliminarTarea = (index, columna) => {
+    const nuevasTareas = [...columnas[columna]];
+    nuevasTareas.splice(index, 1);
+    const nuevasColumnas = {
+      ...columnas,
+      [columna]: nuevasTareas,
+    };
+    setColumnas(nuevasColumnas);
+  };
+  const handleDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+    if (!destination) {
+      return;
+    }
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+    const sourceColumn = columnas[source.droppableId];
+    const destinationColumn = columnas[destination.droppableId];
+    const newColumnas = { ...columnas };
+    if (source.droppableId === destination.droppableId) {
+   
+      const newTasks = Array.from(sourceColumn);
   
 
+      newTasks.splice(source.index, 1);
+
+      newTasks.splice(destination.index, 0, draggableId);
+  
+     
+      newColumnas[source.droppableId] = newTasks;
+    } else {
+  
+      const sourceTasks = Array.from(sourceColumn);
+  
+     
+      const destinationTasks = Array.from(destinationColumn);
+  
+  
+      sourceTasks.splice(source.index, 1);
+  
+
+      destinationTasks.splice(destination.index, 0, draggableId);
+  
+      newColumnas[source.droppableId] = sourceTasks;
+      newColumnas[destination.droppableId] = destinationTasks;
+    }
+    setColumnas(newColumnas);
+  };
   return (
     <HomeContainer>
-      {columnas.map((columna) => (
-        <ColumnaContainer key={columna.titulo}>
-          <Titulo>{columna.titulo}</Titulo>
-          {columna.tarjetas.map((tarjeta) => (
-            <TarjetaContainer key={tarjeta.id}>
-              {/* Renderizar contenido de la tarjeta aquí */}
-            </TarjetaContainer>
-          ))}
-          <Tarjeta/>
-        </ColumnaContainer>
+<DragDropContext onDragEnd={handleDragEnd}>
+
+    {Object.keys(columnas).map((columna) => (
+        <Droppable key={columna} droppableId={columna}>
+          {(provided) => (
+            <ColumnaContainer ref={provided.innerRef} {...provided.droppableProps}>
+              <Titulo>{columna}</Titulo>
+              <TarjetaContainer>
+                <ul>
+                  {columnas[columna].map((tarea, index) => (
+                      <Draggable key={tarea} draggableId={tarea} index={index}>
+                      {(provided) => (
+                          <TareaItem
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          >
+                          <TareaText>{tarea}</TareaText>
+                          <EliminarTareaButton
+                            onClick={() => handleEliminarTarea(index, columna)}
+                            >
+                            X
+                          </EliminarTareaButton>
+                        </TareaItem>
+                      )}
+                    </Draggable>
+                  ))}
+                </ul>
+                <TareaForm onSubmit={(event) => handleSubmit(event, columna)}>
+                  <TareaInput
+                    type="text"
+                    value={nuevaTarea[columnas]}
+                    onChange={handleChange}
+                    />
+                  <AgregarTareaButton type="submit">Agregar</AgregarTareaButton>
+                </TareaForm>
+              </TarjetaContainer>
+              {provided.placeholder}
+            </ColumnaContainer>
+          )}
+        </Droppable>
       ))}
+      </DragDropContext>
     </HomeContainer>
+
   );
 };
 
